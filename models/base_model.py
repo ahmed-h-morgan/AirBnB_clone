286fc40d4ed037b6056847a2d55a3a0263aa0325
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-07-02 00:02:32 +0300

    start creating base_model

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+the Base model
+"""
+
+
+class BaseModel:
+    """
+    the base class
+    that defines all common attributes/methods
+    for other classes:
+    """
+    pass
+
---
+++ tests/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" test base module """
+
+
+import unittest
+from models.base_model import Base
+
+
+class TestBase(unittest.TestCase):
+    """
+    test base class
+    """
+    pass
