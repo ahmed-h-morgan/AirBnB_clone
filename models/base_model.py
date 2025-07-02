3e1369f5756998a36c9e806dbbdba16dc13b6b19
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-07-02 09:00:05 +0000

    task 03 - try 00

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 the Base model
 """
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
@@ -13,5 +14,19 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        
-
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        now = datetime.now()
+        iso_string = now.isoformat()
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = iso_string
+        self.__dict__["updated_at"] = iso_string
+        return self.__dict__
