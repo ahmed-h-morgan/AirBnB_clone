37ea55afa6bb715843623bb99fab168a71592934
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-07-03 14:01:24 +0300

    task 03 - try 03 - update the to_dict model to be more readable

--- models/base_model.py
+++ models/base_model.py
@@ -24,9 +24,9 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        now = datetime.now()
-        iso_string = now.isoformat()
+        # now = datetime.now()
+        # iso_string = now.isoformat()
         self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = iso_string
-        self.__dict__["updated_at"] = iso_string
+        self.__dict__["created_at"] = datetime.isoformat(self.created_at)
+        self.__dict__["updated_at"] = datetime.isoformat(self.updated_at)
         return self.__dict__
