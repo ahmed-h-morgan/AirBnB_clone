4e6577c1efd0a7e60d58c17fa15d97af4b37dd6b
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-07-03 16:27:04 +0300

    task 04 - try 00

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,19 @@ class BaseModel:
     that defines all common attributes/methods
     for other classes:
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
