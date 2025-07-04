cb4be77da3a4b3dbff4f365e614210019496e417
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-07-04 21:44:47 +0300

    task 06 - try 00

--- console.py
+++ console.py
@@ -2,11 +2,34 @@
 """
 the Console model
 """
-import cmd
+import cmd, sys
 
 
 class HBNBCommand(cmd.Cmd):
     """
     The Console class
     """
-    pass
+    prompt = "(hbnb) "
+
+    # def do_help(self, arg):
+    #     """ Give me more info about method """
+    #     return super().do_help(arg)
+
+    def do_quit(self,arg):
+        """ Quit command to exit the program """
+        return True
+    
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    # def emptyline(self):
+    #     """Do nothing when an empty line is entered"""
+    #     pass
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
