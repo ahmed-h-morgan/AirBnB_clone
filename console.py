20e86c0f46dd74942e5f4d881c30876c6fba1bad
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-07-05 13:24:34 +0000

    task 07 - try 00 - create method - Done

--- console.py
+++ console.py
@@ -14,6 +14,9 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
+    valid_classes = {
+        "BaseModel": BaseModel,
+        }
     # def do_help(self, arg):
     #     """ Give me more info about method """
     #     return super().do_help(arg)
@@ -30,9 +33,41 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
+
+    def _split_line(self, line):
+        """ A helper method to split each line """
+        try:
+            line_parts = line.split()
+            return line_parts
+        except ValueError as e:
+            print(f"Splitting line error: {e}")
+            return None
     
-    def do_create(self):
-        pass
+    def _validate_class_name(self, class_name):
+        """ A helper method to validate class name """
+        if not class_name:
+            print("** class name missing **")
+            return False
+        elif class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return False
+        else:
+            return True
+            
+
+    
+    def do_create(self, line):
+        """
+        Creates a new instance of the class
+        saves it (to the JSON file)
+        and prints the id
+        """
+        args = self._split_line(line)
+        if not args or not self._validate_class_name(args[0]):
+            return
+        new_instance = self.valid_classes[args[0]]()
+        print(new_instance.id)
+        new_instance.save()
 
     def do_show(self):
         pass
@@ -47,11 +82,12 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, line):
-        """
-        """
-        accepted_classes = {
-            "BaseModel": BaseModel,
-        }
+        pass
+        # """
+        # """
+        # accepted_classes = {
+        #     "BaseModel": BaseModel,
+        # }
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
